a2032b0149f660d420c0e5e124199dbd720fdf3b
marcoantoniorosario <50590844+marcoantoniorosario@users.noreply.github.com>
2019-05-13 14:16:58 -0300

    dados: completo

--- 1info2-logica/dados.py
+++ 1info2-logica/dados.py
@@ -9,10 +9,31 @@ dado1 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
 print("Dado 1: ", dado1)
 
 dado2 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado1)
+print("Dado 2: ", dado2)
 
 dado3 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado1)
+print("Dado 3: ", dado3)
 
 dado4 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
-print("Dado 1: ", dado1)
+print("Dado 4: ", dado4)
+
+jogador1 = dado1 + dado2
+jogador2 = dado3 + dado4
+
+print('Jogador 1: ', jogador1)
+print('Jogador 2: ', jogador2)
+
+if jogador1 > jogador2:
+    print("Jogador 1 venceu!")
+else:
+    if jogador2 > jogador1:
+        print("Jogador 2 venceu!")
+    else:
+        print("Empate!")
+
+
+
+
+
+
+
