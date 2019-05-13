69a2cffff78e1053ce631669e56bdd08b7ab8be0
marcoantoniorosario <50590844+marcoantoniorosario@users.noreply.github.com>
2019-05-13 13:35:50 -0300

    programa dos dados - 1a versão

---
+++ 1info2-logica/dados.py
@@ -0,0 +1,8 @@
+# Meu primeiro programa
+
+from random import randint  # Importa a função de gerar números aleatórios
+
+print("Olá 1info2!")
+
+dado1 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado1)
