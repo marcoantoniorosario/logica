9ab7b62c825ed96d75cb04724629ec7a0a267a65
marcoantoniorosario <50590844+marcoantoniorosario@users.noreply.github.com>
2019-05-13 13:40:45 -0300

    Dados: incluídos nome do autor e os outros 3 dados

--- 1info2-logica/dados.py
+++ 1info2-logica/dados.py
@@ -1,8 +1,18 @@
 # Meu primeiro programa
+# Marco Antonio do Rosário <marcoantonio.1info@gmail.com
 
 from random import randint  # Importa a função de gerar números aleatórios
 
-print("Olá 1info2!")
+print("Jogo dos dados: ")
 
 dado1 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado1)
+
+dado2 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado1)
+
+dado3 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
+print("Dado 1: ", dado1)
+
+dado4 = randint(1, 6)  # gera um número entre 1 e 6 aleatoriamente
 print("Dado 1: ", dado1)
